57600495ee3e6eb945a0cdac02c748443d612129
Pablo neri <pabloneri66@gmail.com>
2025-01-30 22:32:09 -0300

    Integração API

--- outflows/models.py
+++ outflows/models.py
@@ -4,8 +4,8 @@ from products.models import Product
 
 class Outflow(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='outflows')
-    quantity = models.IntegerField()
-    description = models.TextField(null=True, blank=True)
+    quantity = models.IntegerField(verbose_name='Quantidade')
+    description = models.TextField(null=True, blank=True, verbose_name='Descrição')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- outflows/signals.py
+++ outflows/signals.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Outflow
@@ -10,3 +12,19 @@ def update_product_quantity(sender, instance, created, **kwargs):
             product = instance.product
             product.quantity -= instance.quantity
             product.save()
+
+
+        ### WEBHOOKS --
+@receiver(post_save, sender=Outflow)
+def send_outflows_event(sender, instance, **kwargs):
+    data = {
+        'produto': instance.product.title,
+        'quantidade': instance.quantity,
+        'descrição': instance.description,
+    }
+    requests.post(
+        url='http://localhost:8001/api/v1/webhooks/',
+        json=data,
+    )
+#https://webhook.site/2fe503ef-2559-4449-849a-4ea7dd647a70
+    
